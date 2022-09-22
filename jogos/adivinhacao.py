7e555d26fb823038144e6c83e557eb712561caf6
Matheus Sobreira <matheussobreira2702@gmail.com>
2022-09-22 09:40:47 -0300

    configurando o jogo adivinhação

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,55 +1,61 @@
 import random
-print('-' * 35)
-print('Bem vindo ao jogo de Adivinhação.')
-print('-' * 35)
 
-numero_secreto = random.randint(1,101)
-total_de_tentativas = 0
-pontos = 0
+def jogo_adivinhacao():
 
-print('''Dificuldades:
-[1] - Fácil (20 Tentativas e 2000 pontos)
-[2] - Médio (10 Tentativas e 1000 pontos)
-[3] - Difícil (5 Tentativas e 500 pontos)''')
+    print('-' * 35)
+    print('Bem vindo ao jogo de Adivinhação.')
+    print('-' * 35)
 
-nivel_dificuldade = int(input('Qual nível de dificuldade você deseja? '))
+    numero_secreto = random.randint(1,101)
+    total_de_tentativas = 0
+    pontos = 0
 
-if nivel_dificuldade == 1:
-    total_de_tentativas = 20
-    pontos = 2000
-elif nivel_dificuldade == 2:
-    total_de_tentativas = 10
-    pontos = 1000
-elif nivel_dificuldade == 3:
-    total_de_tentativas = 5
-    pontos = 500
-else:
-    print('Você chutou um número menor que 1 e maior que 3, tera apenas 1 tentativa.')
-    total_de_tentativas = 1
-    pontos = 2000
-    
-for rodada in range(1, total_de_tentativas+1):
-    print(f'Tentativa {rodada} de {total_de_tentativas}')
-    chute_usuario = int(input('Chute um número entre 1 e 100: '))
-    print('Voce digitou', chute_usuario)
+    print('''Dificuldades:
+    [1] - Fácil (20 Tentativas e 2000 pontos)
+    [2] - Médio (10 Tentativas e 1000 pontos)
+    [3] - Difícil (5 Tentativas e 500 pontos)''')
 
-    if chute_usuario < 1 or chute_usuario > 100:
-        print('Você deve digitar um número entre 1 e 100')
-        continue
+    nivel_dificuldade = int(input('Qual nível de dificuldade você deseja? '))
 
-    acerto = numero_secreto == chute_usuario
-    maior = chute_usuario > numero_secreto
-    menor = chute_usuario < numero_secreto
-
-    if acerto:
-        print(f'Parabéns, você acertou e fez {pontos} pontos.')
-        break
+    if nivel_dificuldade == 1:
+        total_de_tentativas = 20
+        pontos = 2000
+    elif nivel_dificuldade == 2:
+        total_de_tentativas = 10
+        pontos = 1000
+    elif nivel_dificuldade == 3:
+        total_de_tentativas = 5
+        pontos = 500
     else:
-        if maior:
-            print('Não foi dessa vez, você chutou um número maior do que o número secreto.')
-        elif menor:
-            print('Não foi dessa vez, você chutou um número menor do que o número secreto.')
-        pontos_perdidos = abs(numero_secreto - chute_usuario)
-        pontos -= pontos_perdidos  
+        print('Você chutou um número menor que 1 e maior que 3, tera apenas 1 tentativa.')
+        total_de_tentativas = 1
+        pontos = 2000
+        
+    for rodada in range(1, total_de_tentativas+1):
+        print(f'Tentativa {rodada} de {total_de_tentativas}')
+        chute_usuario = int(input('Chute um número entre 1 e 100: '))
+        print('Voce digitou', chute_usuario)
 
-    print()
+        if chute_usuario < 1 or chute_usuario > 100:
+            print('Você deve digitar um número entre 1 e 100')
+            continue
+
+        acerto = numero_secreto == chute_usuario
+        maior = chute_usuario > numero_secreto
+        menor = chute_usuario < numero_secreto
+
+        if acerto:
+            print(f'Parabéns, você acertou e fez {pontos} pontos.')
+            break
+        else:
+            if maior:
+                print('Não foi dessa vez, você chutou um número maior do que o número secreto.')
+            elif menor:
+                print('Não foi dessa vez, você chutou um número menor do que o número secreto.')
+            pontos_perdidos = abs(numero_secreto - chute_usuario)
+            pontos -= pontos_perdidos  
+
+        print()
+
+if __name__ == "__main__":
+    jogo_adivinhacao()
---
+++ jogos/forca.py
@@ -0,0 +1,8 @@
+def jogo_forca():
+
+    print('-' * 28)
+    print('Bem vindo ao jogo da Forca.')
+    print('-' * 28)
+
+if __name__ == "__main__":
+    jogo_forca()
